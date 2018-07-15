346089df85fa219577f596c3b4d4b786b932bd0a
Aquib Jawed <aquib_j@yahoo.com>
2018-07-15 22:39:05 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+	$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+
+	$(".dropdown-trigger").dropdown();
+
+
+	$('.modal').modal();
+});
